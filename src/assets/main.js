9a77200ef3bbc14060502c195e2f06f17492e659
Ben Wang <ben.wang@sonru.com>
2017-06-23 17:23:06 +0100

    improved main.js

--- src/assets/main.js
+++ src/assets/main.js
@@ -18,30 +18,40 @@ function guess() {
 //implement new functions here
 
 function setHiddenFields() {
-    answer = Math.floor( Math.random() * 10000 ).toString();
-    while(answer.length < 4) {
-        answer = '0' + answer;
+    newValue = Math.floor( Math.random() * 10000 ).toString();
+    while(newValue.length < 4) {
+      newValue = '0' + newValue;
     }
+    answer.value = newValue;
     attempt = 0;
 }
-
-function setMessage(message) {
-    document.getElementById('message').innerHTML = message;
-
-}
-
-function validateInput(input) {
-    if(input.length == 4){
-        return true;
-    } else {
-        setMessage("Guesses must be exactly 4 characters long.");
-        return false;
-    }
-}
-
-function getResults(results) {
-    var initDiv;
-    for(i = 0; i < results.length; i++) {
-        initDiv.append('<div class="row"><span class="col-md-6">' + results[i] + '</span><div class="col-md-6">')
-    }
-}
+//
+// function setMessage(message) {
+//     document.getElementById('message').innerHTML = message;
+//
+// }
+//
+// function validateInput(input) {
+//     if(input.length == 4){
+//         return true;
+//     } else {
+//         setMessage("Guesses must be exactly 4 characters long.");
+//         return false;
+//     }
+// }
+//
+// function getResults(result) {
+//     var resultsDiv = "<div class='row'>";
+//     for(i = 0; i < result.length; i++) {
+//         if(result[i] == answer[i]){
+//           tempSpan = "<span class='col-md-6 glyphicon glyphicon-ok'>" + result[i] + "</span>"
+//         } else if( result[i] != answer[i] && answer.includes(result[i])) {
+//           tempSpan = "<span class='col-md-6 glyphicon glyphicon-transfer'>" + result[i] + "</span>"
+//         } else {
+//           tempSpan = "<span class='col-md-6 glyphicon glyphicon-remove'>" + result[i] + "</span>"
+//         }
+//
+//         resultsDiv += tempSpan;
+//     }
+//     resultsDiv +=
+// }
