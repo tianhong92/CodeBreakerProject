84632c1c060464ae1c88419fa338e64b4781d56f
Ben Wang <ben.wang@sonru.com>
2017-07-07 14:57:37 +0100

    fix empty

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,7 +4,7 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
-    if(answer.text == '' || attempt.text == ''){
+    if(answer.text == '' && attempt.text == ''){
         setHiddenFields();
     }
 
